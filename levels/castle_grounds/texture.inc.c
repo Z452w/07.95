06048d41a92b8203f73a7832001f48f9560ecb8c
Z452w <alysonjoycejaninen@gmail.com>
2026-02-17 00:40:37 -0500

    Update texture.inc.c

--- levels/castle_grounds/texture.inc.c
+++ levels/castle_grounds/texture.inc.c
@@ -1,16 +1,16 @@
 // 0x07000000 - 0x07001000
 ALIGNED8 static const u8 castle_grounds_seg7_texture_07000000[] = {
-#include "levels/castle_grounds/0.rgba16.inc.c"
+#include "actors/mario/meye00_txt.rgba16.inc.c"
 };
 
 // 0x07001000 - 0x07002000
 ALIGNED8 static const u8 castle_grounds_seg7_texture_07001000[] = {
-#include "levels/castle_grounds/1.rgba16.inc.c"
+#include "actors/mario/meye00_txt.rgba16.inc.c"
 };
 
 // 0x07002000 - 0x07003000
 ALIGNED8 static const u8 castle_grounds_seg7_texture_07002000[] = {
-#include "levels/castle_grounds/2.rgba16.inc.c"
+#include "actors/mario/meye00_txt.rgba16.inc.c"
 };
 
 /*
@@ -38,7 +38,7 @@ ALIGNED8 static const u8 castle_grounds_seg7_texture_07006800[] = {
 
 // 0x07008000 - 0x07009000
 ALIGNED8 static const u8 castle_grounds_seg7_texture_07008000[] = {
-#include "levels/castle_grounds/7.rgba16.inc.c"
+#include "actors/mario/meye00_txt.rgba16.inc.c"
 };
 
 // 0x07009000 - 0x0700A800
