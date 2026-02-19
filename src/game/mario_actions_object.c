2023335309290ecd0a4b48af62a5c1d9c6de7897
Z452w <alysonjoycejaninen@gmail.com>
2026-02-19 12:32:03 -0500

    Update mario_actions_object.c

--- src/game/mario_actions_object.c
+++ src/game/mario_actions_object.c
@@ -18,7 +18,8 @@ void animated_stationary_ground_step(struct MarioState *m, s32 animation, u32 en
 }
 
 s32 mario_update_punch_sequence(struct MarioState *m) {
-    
+u32 endAction = (m->action & ACT_FLAG_MOVING) ? ACT_WALKING : ACT_IDLE;
+
     if (mario_check_object_grab(m)) {
         return TRUE;
     }
@@ -32,9 +33,8 @@ s32 act_punching(struct MarioState *m) {
     }
 
     mario_update_punch_sequence(m);
-
-    u32 targetAction = (m->action & ACT_FLAG_MOVING) ? ACT_WALKING : ACT_IDLE;
-    return set_mario_action(m, targetAction, 0);
+    perform_ground_step(m);
+    return FALSE;
 }
 
 s32 act_picking_up(struct MarioState *m) {
