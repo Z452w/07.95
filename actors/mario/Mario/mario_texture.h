3062923ac3a1e8ef56e255a27d39756de949ee05
Z452w <alysonjoycejaninen@gmail.com>
2026-02-20 20:12:31 -0500

    Update mario_texture.h

--- actors/mario/Mario/mario_texture.h
+++ actors/mario/Mario/mario_texture.h
@@ -11,52 +11,52 @@ ALIGNED8 static const Texture m_mark_txt[] = {
 };
 
 ALIGNED8 static const Texture m_momi_txt[] = {
-#include "actors/mario/meye00_txt.rgba16.inc.c"
+#include "actors/mario/m_momi_txt.rgba16.inc.c"
 };
 
 ALIGNED8 static const Texture m_hige_txt[] = {
-#include "actors/mario/meye00_txt.rgba16.inc.c"
+#include "actors/mario/m_hige_txt.rgba16.inc.c"
 };
 
 /********************************************************************************/
 /*	Mario eye texture data.														*/
 /********************************************************************************/
 ALIGNED8 static const Texture meye1_txt[] = {
-#include "actors/mario/meye00_txt.rgba16.inc.c"
+#include "actors/mario/meye1_txt.rgba16.inc.c"
 };
 
 ALIGNED8 static const Texture meye2_txt[] = {
-#include "actors/mario/meye00_txt.rgba16.inc.c"
+#include "actors/mario/meye2_txt.rgba16.inc.c"
 };
 
 ALIGNED8 static const Texture meye3_txt[] = {
-#include "actors/mario/meye00_txt.rgba16.inc.c"
+#include "actors/mario/meye3_txt.rgba16.inc.c"
 };
 
 ALIGNED8 static const Texture meye4_txt[] = {
-#include "actors/mario/meye00_txt.rgba16.inc.c"
+#include "actors/mario/meye4_txt.rgba16.inc.c"
 };
 
 ALIGNED8 static const Texture meye5_txt[] = {
-#include "actors/mario/meye00_txt.rgba16.inc.c"
+#include "actors/mario/meye5_txt.rgba16.inc.c"
 };
 
 ALIGNED8 static const Texture meye_R_txt[] = {
-#include "actors/mario/meye00_txt.rgba16.inc.c"
+#include "actors/mario/meye_R_txt.rgba16.inc.c"
 };
 
 ALIGNED8 static const Texture meye_L_txt[] = {
-#include "actors/mario/meye00_txt.rgba16.inc.c"
+#include "actors/mario/meye_L_txt.rgba16.inc.c"
 };
 
 ALIGNED8 static const Texture meye_up_txt[] = {
-#include "actors/mario/meye00_txt.rgba16.inc.c"
+#include "actors/mario/meye_up_txt.rgba16.inc.c"
 };
 
 ALIGNED8 static const Texture meye_under_txt[] = {
-#include "actors/mario/meye00_txt.rgba16.inc.c"
+#include "actors/mario/meye_under_txt.rgba16.inc.c"
 };
 
 ALIGNED8 static const Texture meye_down_txt[] = {
-#include "actors/mario/meye00_txt.rgba16.inc.c"
+#include "actors/mario/meye_up_txt.rgba16.inc.c"
 };
