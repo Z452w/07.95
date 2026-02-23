85b6cbf8929ef63b62174596d49412ffee3a3f1e
Z452w <alysonjoycejaninen@gmail.com>
2026-02-22 19:12:02 -0500

    Update mario_actions_airborne.c

--- src/game/mario_actions_airborne.c
+++ src/game/mario_actions_airborne.c
@@ -713,8 +713,7 @@ s32 act_steep_jump(struct MarioState *m) {
 }
 
 s32 act_ground_pound(struct MarioState *m) {
-    set_mario_action(m, ACT_BEGIN_SLIDING, 0);
-    return FALSE;
+    set_mario_action(m, ACT_HOLD_JUMP, 0);
 }
 
 s32 act_burning_jump(struct MarioState *m) {
