9abe3099917372a059052bafa19e1c99fd708fa7
Z452w <alysonjoycejaninen@gmail.com>
2026-02-26 00:40:37 -0500

    Update mario_actions_automatic.c

--- src/game/mario_actions_automatic.c
+++ src/game/mario_actions_automatic.c
@@ -568,7 +568,7 @@ s32 act_ledge_grab(struct MarioState *m) {
 
     heightAboveFloor = m->pos[1] - find_floor_height_relative_polar(m, -0x8000, 30.0f);
     if (hasSpaceForMario && heightAboveFloor < 100.0f) {
-        return set_mario_action(m, ACT_IDLE, 0);
+        return set_mario_action(m, ACT_LEDGE_CLIMB_FAST, 0);
     }
 
     if (m->actionArg == 0) {
