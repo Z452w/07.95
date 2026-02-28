a300bb641e1d97ca640a756b8af846f176439bdb
Z452w <alysonjoycejaninen@gmail.com>
2026-02-28 11:39:20 -0500

    Update script.c

--- levels/ttc/script.c
+++ levels/ttc/script.c
@@ -7,24 +7,12 @@
 #include "segment_symbols.h"
 #include "level_commands.h"
 
-#include "game/level_update.h"
-
-#include "levels/scripts.h"
-
-#include "actors/common1.h"
-
-#include "make_const_nonconst.h"
-
 static const LevelScript script_func_local_1[] = {
     RETURN(),
 };
 
 const LevelScript level_ttc_entry[] = {
     INIT_LEVEL(),
-    SLEEP(/*frames*/ 0),
-    EXIT_AND_PAUSE(),
-    END_AREA(),
     FREE_LEVEL_POOL(),
-    SLEEP(/*frames*/ 0),
-    EXIT_AND_PAUSE(),
+    EXIT(),
 };
